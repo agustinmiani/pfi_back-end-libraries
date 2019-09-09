2505dabaabe1d0887495478c5247de17a75b24c2
Agustin Miani <agustinmiani@gmail.com>
2019-09-08 22:40:02 -0300

    Add new constructor

--- src/main/java/dto/reserve/SportFieldReservesDTO.java
+++ src/main/java/dto/reserve/SportFieldReservesDTO.java
@@ -2,6 +2,7 @@ package dto.reserve;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 public class SportFieldReservesDTO {
     private List<ReserveDTO> ownReserves = new ArrayList<ReserveDTO>();
     private List<ReserveDTO> otherReserves = new ArrayList<ReserveDTO>();
