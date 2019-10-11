ee617a2bff3bcac6ff89990522d0c3da37809dfc
Agustin Miani <agustinmiani@gmail.com>
2019-10-10 21:40:52 -0300

    Add Reserve Response

---
+++ src/main/java/dto/reserve/ReserveResponseDTO.java
@@ -0,0 +1,15 @@
+package dto.reserve;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by amiani on 2019-10-10
+ */
+@NoArgsConstructor
+@Getter
+@Setter
+public class ReserveResponseDTO {
+    private AbstractReserveDTO reserve;
+}
