fba6705995f4d8545a6c87a9ed60070d8474b525
Agustin Miani <agustinmiani@gmail.com>
2019-10-10 23:55:59 -0300

    Add builder on response

--- src/main/java/dto/reserve/ReserveResponseDTO.java
+++ src/main/java/dto/reserve/ReserveResponseDTO.java
@@ -1,5 +1,6 @@
 package dto.reserve;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.Setter;
  * Created by amiani on 2019-10-10
  */
 @NoArgsConstructor
+@Builder
 @Getter
 @Setter
 public class ReserveResponseDTO {
